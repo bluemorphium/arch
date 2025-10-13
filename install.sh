fcedaedb80fbe8d193e817751e86a297f91a5c1f
bluemorphium <87581232+bluemorphium@users.noreply.github.com>
2025-10-13 11:53:41 +0200

    \

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 
 echo 'starting to install programs'
 
-sudo pacman -S --needed / 
-kitty curl wget git fastfetch btop htop networkmanager fzf tmux /
-man-db openssh vsftpd mtr usbutils lshw /
-dysk yazi fwupd wireguard-tools /
-hyprland waybar wofi swaync hyprshot hyprlock hypridle hyprpaper /
-network-manager-applet otf-font-awesome bluez bluez-utils bluetui blueman veracrypt pipewire-pulse pavucontrol /
-dolphin kate firefox code vlc vlc-plugins-all /
-qalculate-qt libreoffice-still xournalpp /
+sudo pacman -S --needed \
+kitty curl wget git fastfetch btop htop networkmanager fzf tmux \
+man-db openssh vsftpd mtr usbutils lshw \
+dysk yazi fwupd wireguard-tools \
+hyprland waybar wofi swaync hyprshot hyprlock hypridle hyprpaper \
+network-manager-applet otf-font-awesome bluez bluez-utils bluetui blueman veracrypt pipewire-pulse pavucontrol \
+dolphin kate firefox code vlc vlc-plugins-all \
+qalculate-qt libreoffice-still xournalpp \
 yubikey-manager pam-u2f
 
 echo 'done'
