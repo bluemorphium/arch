30812ddec4e76d1b525bdaf0fb84db25ffbd4728
bluemorphium <87581232+bluemorphium@users.noreply.github.com>
2025-10-13 15:35:27 +0200

    nextcloud

--- install.sh
+++ install.sh
@@ -11,6 +11,6 @@ hyprland waybar wofi swaync hyprshot hyprlock hypridle hyprpaper \
 network-manager-applet otf-font-awesome bluez bluez-utils bluetui blueman veracrypt pipewire-pulse pavucontrol \
 dolphin kate firefox thunderbird code vlc vlc-plugins-all \
 qalculate-qt libreoffice-still xournalpp \
-yubikey-manager pam-u2f
+yubikey-manager pam-u2f nextcloud
 
 echo 'done'
