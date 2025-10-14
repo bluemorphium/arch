1263ebd182ca9954e6c6811fade1e35de2e5c132
bluemorphium <87581232+bluemorphium@users.noreply.github.com>
2025-10-14 09:07:57 +0200

    archlinux-xdg-menu

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo pacman -S --needed \
 kitty curl wget git fastfetch btop htop networkmanager fzf tmux \
 man-db openssh vsftpd mtr usbutils lshw \
 dysk yazi fwupd wireguard-tools \
-hyprland waybar wofi swaync hyprshot hyprlock hypridle hyprpaper xdg-desktop-portal-hyprland xdg-desktop-portal-gtk \
+hyprland waybar wofi swaync hyprshot hyprlock hypridle hyprpaper xdg-desktop-portal-hyprland xdg-desktop-portal-gtk archlinux-xdg-menu \
 network-manager-applet otf-font-awesome bluez bluez-utils bluetui blueman veracrypt pipewire-pulse pavucontrol \
 dolphin kate firefox thunderbird code vlc vlc-plugins-all \
 qalculate-qt libreoffice-still xournalpp \
