1c1c7970e4734c4d948241a772ecbb2732170e23
bluemorphium <87581232+bluemorphium@users.noreply.github.com>
2025-10-17 08:38:04 +0200

    update
    Updated installation script to include additional packages and modify echo statements.

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 
-echo 'starting to install programs'
-
+echo 'starting to install Main programs'
 sudo pacman -S --needed --noconfirm \
-kitty curl wget git fastfetch btop htop networkmanager fzf tmux \
+kitty curl wget git fastfetch btop htop powertop networkmanager fzf tmux screen picocom \
 man-db openssh vsftpd mtr usbutils lshw nfs-utils \
 dysk yazi ncdu rsync fwupd wireguard-tools \
-hyprland waybar wofi swaync hyprshot hyprlock hypridle hyprpaper \
+hyprland waybar wofi swaync hyprshot hyprlock hypridle hyprpaper otf-font-awesome \
 xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr archlinux-xdg-menu kde-cli-tools \
-network-manager-applet otf-font-awesome bluez bluez-utils bluetui blueman veracrypt pipewire-pulse pavucontrol \
-dolphin kate firefox thunderbird code vlc vlc-plugins-all \
+network-manager-applet bluez bluez-utils bluetui blueman pipewire-pulse pavucontrol \
+dolphin kate firefox thunderbird veracrypt code vlc vlc-plugins-all \
 qalculate-qt libreoffice-still xournalpp \
-yubikey-manager pam-u2f nextcloud-client
+
+echo 'starting to install UIniKo programs'
+sudo pacman -S --needed --noconfirm \
+mattermost-desktop yubikey-manager pam-u2f nextcloud-client \
 
 echo 'done'
