def3ede3ba1bffc71358606f9744b4865b9a320b
bluemorphium <87581232+bluemorphium@users.noreply.github.com>
2025-10-17 10:00:12 +0200

    qemu
    Added installation of Qemu programs and updated formatting.

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo 'starting to install Main programs'
 sudo pacman -S --needed --noconfirm \
 kitty curl wget git fastfetch btop htop powertop networkmanager fzf tmux screen picocom \
@@ -10,10 +9,14 @@ hyprland waybar wofi swaync hyprshot hyprlock hypridle hyprpaper otf-font-awesom
 xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr archlinux-xdg-menu kde-cli-tools \
 network-manager-applet bluez bluez-utils bluetui blueman pipewire-pulse pavucontrol \
 dolphin kate firefox thunderbird veracrypt code vlc vlc-plugins-all \
-qalculate-qt libreoffice-still xournalpp \
+qalculate-qt libreoffice-still xournalpp
 
 echo 'starting to install UIniKo programs'
 sudo pacman -S --needed --noconfirm \
-mattermost-desktop yubikey-manager pam-u2f nextcloud-client \
+mattermost-desktop yubikey-manager pam-u2f nextcloud-client
+
+echo 'starting to install Qemu programs'
+sudo pacman -S --needed --noconfirm \
+qemu virt-manager libvirt virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat ebtables libguestfs dmidecode
 
 echo 'done'
